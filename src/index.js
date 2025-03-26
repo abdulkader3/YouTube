5c4e5abc001b68c68f03f3bbdedb437d9ab01ef6
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-03-26 18:08:12 +0600

    app.js apiErrors apiResponse asyncHandlers

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import connectDB from './db/index.js';
 dotenv.config('./env')
 
 connectDB()
+
+.then(()=>{
+    app.listen(process.env.PORT || 9000, ()=>{
+        console.log(`app listening on port ${process.env.PORT}`)
+    })
+})
+
+.catch((err)=>{
+    console.log('Mongodb connection error' , err)
+})
---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,7 @@
+const asyncHandlers = (reqHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(reqHandler(req,res,next)).catch((err)=>(err))
+    }
+}
+
+export{asyncHandlers}
