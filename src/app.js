a0fa1a79f312d7e4ddd383da4c47849cbeba4246
Abdul Kader <168454554+abdulkader3@users.noreply.github.com>
2025-04-03 14:28:30 +0600

    cors is a Function

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import cors from 'cors';
 
 const app = express()
 
-app.use(express.cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credential: true
+    credentials: true
 }))
 
 app.use(express.json({limit: '16kb'}))
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config('./env')
 
 connectDB()
 
 .then(()=>{
-    app.listen(process.env.PORT || 9000, ()=>{
-        console.log(`app listening on port ${process.env.PORT}`)
+    const PORT = (process.env.PORT || 9000)
+    app.listen(PORT , ()=>{
+        console.log(`app listening on port ${PORT}`)
     })
 })
 
